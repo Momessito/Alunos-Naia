e46a500b4eac95f2a2835f2df350cc8b9fa4d341
Felipe Momesso <116132380+Momessito@users.noreply.github.com>
2023-09-04 10:48:01 -0300

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -23,6 +23,14 @@ export default function Quiz(){
   <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdZ-WsjnGzsyYwSKJoWbAPF11zDCupSFQcnlW_rV8EHk6Krrg/viewform?embedded=true" width="100%" height="400px " frameborder="0" marginheight="0" marginwidth="0">Carregando…</iframe>
   </div>
 </div>
+            <div className="collapse collapse-arrow bg-base-200">
+  <input type="radio" name="my-accordion-2" /> 
+  <div className="collapse-title text-xl font-medium">
+    Quiz - Internet
+  </div>
+  <div className="collapse-content"> 
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScfRW6NcY7Rv1WyioEXqE7oSio1IcXY_lBdJH88dZfiP3ubYg/viewform?embedded=true" width="100%" height="1174" frameborder="0" marginheight="0" marginwidth="0">Carregando…</iframe>  </div>
+</div>
         </div>
     );
 }
